e3a041cd6f4076ece22f7fa1d9eca8536660a980
Andrey <81855011+dhdbd3uwh@users.noreply.github.com>
2021-11-20 03:06:48 +0300

    Update prog_uz_v1.5.4.py

--- prog_uz_v1.5.4.py
+++ prog_uz_v1.5.4.py
@@ -9,23 +9,25 @@ Created on Sat Oct  2 20:58:45 2021
 """
 import openpyxl, shutil, sys
 #from progress.bar import IncrementalBar
-from myfunc import Rabota_nad_vsem_Failom, Print_File_Final, BD_Cadet_Data
-from variables import file_spisok_kadet, period_uval,vudacha, CADET_DATA2
+from myfunc import BD_Cadet_Data
+from variables import file_spisok_kadet, period_uval, vudacha, CADET_DATA2
+from funcGUI import Print_Window
 
 
 #______________________________________________________________________________
 def XL_START ():
     shutil.copyfile("InfTeh/uvol_zap_initial.xlsx", "InfTeh/uvol_zap_final.xlsx")
     
-    #запуск функции изъятия данных из БД
+    # запуск функции изъятия данных из БД
     BD_Cadet_Data()
-    
-    # запуск главной функции и функции печати
-    Rabota_nad_vsem_Failom(len(CADET_DATA2))
-    print('Подключение к принтеру...')
-    Print_File_Final(len(CADET_DATA2))
+    # запуск интерфейса
+    Print_Window()
 
-    print('________ \nCOMPLITE')
+    # # запуск главной функции и функции печати
+    # Rabota_nad_vsem_Failom(len(CADET_DATA2))
+    # print('Подключение к принтеру...')
+    # Print_File_Final(len(CADET_DATA2))
+
     sys.exit(0)
 #______________________________________________________________________________
 
