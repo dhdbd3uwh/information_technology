4211d58f878ba091c6021e20edc85d7029d8bc15
Andrey <81855011+dhdbd3uwh@users.noreply.github.com>
2021-12-23 10:41:02 +0300

    Update main.py

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,6 +1,8 @@
 import os
 from proguz import XL_START
-os.system('python DialogPassword_main.py')
+from DialogPassword_main import zapusk
 
+#os.system('python DialogPassword_main.py')
+zapusk()
 XL_START()
 
